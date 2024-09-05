722bb05ef9d91f9f88a6f232fa397afac49033e0
kumaroid <andr-ushka2@yandex.ru>
2024-09-05 22:50:58 +0300

    finally

--- equations.fsx
+++ equations.fsx
@@ -1,28 +1,39 @@
-// Определите функции для решение алгебраических уравнений
+let e = 2.718281
+let eps = 0.0000001
 
-let dichotomy f a b = 0.
+let rec dichotomy f a b = 
+    let mid = (a + b) / 2.0
+    if abs(f mid) < eps then mid
+    else
+        if f(a) * f(b) < 0.0 then 
+            if f(mid) * f(a) > 0.0 then dichotomy f mid b
+            else dichotomy f a mid
+        else mid
 
-let iterations phi x0 = 0.
+let rec iterations phi x0 =
+    if abs(x0 - phi(x0)) < eps then x0
+    else iterations phi (phi x0)
 
-let newthon f f' x0 = 0.
-// используйте функцию 'iterations'
+let rec newthon f f' x0 =
+    let phi x= x - f(x)/f'(x)
+    iterations phi x0
 
 // Решите 3 уравнения (начиная со своего номера варианта) с использованием 3-х методов
-let f1 = ...
-let f2 = ...
-let f3 = ...
+let f1 x = 2.0 * x * sin(x) - cos(x)
+let f2 x = e ** x + (1.0 + e ** (2.0 * x)) ** 0.5 - 2.0
+let f3 x = log(x) - x + 1.8
 
-let f1' = ...
-let f2' = ...
-let f3' = ...
+let f1' x = 3.0 * sin(x) + 2.0 * x * cos(x)
+let f2' x = (e ** x * (e ** (2.0 * x) + 1.0) ** 0.5 + e ** (2.0 * x)) / ((e ** (2.0 * x) + 1.0) ** 0.5)
+let f3' x = - (x - 1.0) / x
 
-let phi1 = ...
-let phi2 = ...
-let phi3 = ...
+let phi1 x = x - f1(x) / f1'(x)
+let phi2 x = x - f2(x) / f2'(x)
+let phi3 x = x - f3(x) / f3'(x)
 
 let main = 
-    printfn "%10.5f  %10.5f  %10.5f" (dichotomy f1 0. 1.) (iterations phi1 0.) (newthon f1 f1' 1.)
-    printfn "%10.5f  %10.5f  %10.5f" (dichotomy f2 0. 1.) (iterations phi2 0.) (newthon f2 f2' 1.)
-    printfn "%10.5f  %10.5f  %10.5f" (dichotomy f3 0. 1.) (iterations phi3 0.) (newthon f3 f3' 1.)
+    printfn " | %10.5f | %10.5f | %10.5f|" (dichotomy f1 0.4 1.) (iterations phi1 1.) (newthon f1 f1' 1.)
+    printfn " |%10.5f  | %10.5f | %10.5f|" (dichotomy f2 -1. 0.) (iterations phi2 0.) (newthon f2 f2' 0.)
+    printfn " |%10.5f  | %10.5f | %10.5f|" (dichotomy f3 2. 3.) (iterations phi3 3.) (newthon f3 f3' 3.)
 
  
--- taylor.fsx
+++ taylor.fsx
@@ -1,25 +1,34 @@
-// Print a table of a given function f, computed by taylor series
-
-// function to compute
-let f = sin
+let f x = sin(x) * sin(x)
 
 let a = 0.0
 let b = 1.0
 let n = 10
+let eps = 0.000001
 
+let factorial num =
+    [1..num] |> Seq.fold (fun acc n -> acc * n) 1
 // Define a function to compute f using naive taylor series method
-let taylor_naive = f
+let taylor_naive x = 
+    let rec naive n acc term =
+        if abs term < eps then acc, n
+        else
+            let term: float = ((-1.0) ** (n - 1.0)) * (2.0 ** (2.0 * n - 1.0)) * (x ** (2.0 * n)) / float(factorial (2 * int n))
+            naive (n + 1.0) (acc + term) term
+    naive 1. (0.) (2.0 * x ** 2.0 / 2.0)
 
-
-// Define a function to do the same in a more efficient way
-let taylor = f
+let taylor x = 
+    let rec taylor_0 n acc term = 
+        if abs term < eps then acc, n
+        else
+          let term = term * (-1.0) * (4.0) * (x ** 2.0) / (2.0 * n + 1.0) / (2.0 * n + 2.0)
+          taylor_0 (n + 1.0) (acc + term) term
+    taylor_0 1. (2.0 * x ** 2.0 /  2.0) (2.0 * x ** 2.0 / 2.0)
 
 let main =
-   for i=0 to n do
-     let x = a+(float i)/(float n)*(b-a)
-     printfn "%5.2f  %10.6f  %10.6f   %10.6f" x (f x) (taylor_naive x) (taylor x)
-// make sure to improve this table to include the required number of iterations
-// for each of the methods
+   for i = 0 to n do
+    let x = a + (float i) / (float n) * (b-a)
+    let naive, terms1 = taylor_naive x
+    let taylor, terms2 = taylor x
+    printfn "%5.2f   %10.6f  %10.6f  %.0f  %10.6f  %.0f" x (f x) naive terms1 taylor terms2
 
 main
-
